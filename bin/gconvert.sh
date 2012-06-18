47056fad61e50ee31a75dd1c0e50c8c7a792cfc9
flashsword20 <flashsword20@gmail.com>
2012-06-18 18:24:33 +0800

    commit

---
+++ bin/gconvert.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if [ -z $2 ] 
+then 
+echo  "Usage: gconvert [input dianlog file] [output wordpress file]"
+else
+java -cp groovy.jar:dianlog-convertor.jar com.gugugua.DianlogConverter $1 $2
+fi
