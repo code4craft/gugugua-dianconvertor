6181378a4f25d2260f012a0d8de9e35afbbb7a21
flashsword20 <flashsword20@gmail.com>
2012-06-18 18:54:28 +0800

    bat

--- bin/gconvert.bat
+++ bin/gconvert.bat
@@ -1,6 +1,6 @@
-if "%2"=="" (
+@if "%2"=="" (
  echo "Usage: gconvert.[sh|bat] [input dianlog file] [output wordpress file]"
 )
 else (
-java -cp groovy.jar:dianlog-convertor.jar com.gugugua.DianlogConverter "%1" "%2"
+java -cp groovy.jar;dianlog-convertor.jar com.gugugua.DianlogConverter "%1" "%2"
 )
